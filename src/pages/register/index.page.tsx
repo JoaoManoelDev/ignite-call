0e1ee0d7ef228843b53f4cfa81878ddcdfac18ba
João <joaomanoeldev@gmail.com>
2022-12-24 13:15:24 -0300

    Visual structure of the register page

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,36 @@
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Form, Header, RegisterContainer } from './styles'
+
+export default function Register() {
+  return (
+    <RegisterContainer>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+
+      <Form as="form">
+        <label>
+          <Text size="sm">Nome de usuário</Text>
+          <TextInput prefix="ignite.com/" placeholder="seu-usuário" />
+        </label>
+
+        <label>
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu nome" />
+        </label>
+
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </Form>
+    </RegisterContainer>
+  )
+}
