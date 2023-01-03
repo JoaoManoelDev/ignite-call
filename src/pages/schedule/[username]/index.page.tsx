ee0b8748aa208951d7cb3ba79a9bb8b4d4016616
João <joaomanoeldev@gmail.com>
2023-01-03 15:33:19 -0300

    Schedule page

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,58 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '../../../lib/prisma'
+import { ContainerSchedule, UserHeader } from './styles'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <ContainerSchedule>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}l</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+    </ContainerSchedule>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
