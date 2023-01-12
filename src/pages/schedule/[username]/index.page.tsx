4e92666f5a29bcd36ad6261e70b32319b4cf2c3f
João <joaomanoeldev@gmail.com>
2023-01-12 17:13:58 -0300

    Refresh token Google

--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -17,7 +17,7 @@ export default function Schedule({ user }: ScheduleProps) {
     <ContainerSchedule>
       <UserHeader>
         <Avatar src={user.avatarUrl} />
-        <Heading>{user.name}l</Heading>
+        <Heading>{user.name}</Heading>
         <Text>{user.bio}</Text>
       </UserHeader>
 
